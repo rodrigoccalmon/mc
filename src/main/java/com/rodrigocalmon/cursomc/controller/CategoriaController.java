646bbe39c7b4b2e4c7b59901aa1bd2bdea5f694c
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-01-24 15:20:24 -0300

    REST testado

---
+++ src/main/java/com/rodrigocalmon/cursomc/controller/CategoriaController.java
@@ -0,0 +1,16 @@
+package com.rodrigocalmon.cursomc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaController {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando";
+	}
+	
+}
