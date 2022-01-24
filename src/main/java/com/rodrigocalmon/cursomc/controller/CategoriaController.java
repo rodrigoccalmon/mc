a7c4d2700fa3a9e479fbac11a9cd80f3ab087f05
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-01-24 16:35:38 -0300

    Testando a primeira classe de domínio - categoria

--- src/main/java/com/rodrigocalmon/cursomc/controller/CategoriaController.java
+++ src/main/java/com/rodrigocalmon/cursomc/controller/CategoriaController.java
@@ -1,16 +1,27 @@
 package com.rodrigocalmon.cursomc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rodrigocalmon.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+
+		return lista;
 	}
-	
+
 }
