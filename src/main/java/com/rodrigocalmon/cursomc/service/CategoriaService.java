5b25f66925b1d2ae85768ea40c74ffc4ea8cd559
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-01-25 12:43:48 -0300

    exception categoria

--- src/main/java/com/rodrigocalmon/cursomc/service/CategoriaService.java
+++ src/main/java/com/rodrigocalmon/cursomc/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.rodrigocalmon.cursomc.domain.Categoria;
 import com.rodrigocalmon.cursomc.repository.CategoriaRepository;
+import com.rodrigocalmon.cursomc.service.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,8 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+	public Categoria find(Integer id) { 
+		 Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+		} 
 }
