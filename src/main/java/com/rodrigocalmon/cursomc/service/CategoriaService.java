cee62063f13162c8f02fbcd69b27380f75795875
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-01-31 15:40:23 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/rodrigocalmon/cursomc/service/CategoriaService.java
+++ src/main/java/com/rodrigocalmon/cursomc/service/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigocalmon.cursomc.domain.Categoria;
@@ -36,5 +39,10 @@ public class CategoriaService {
 	}
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), 
+				orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
