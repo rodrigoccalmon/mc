0d3b74ec6012c775110ece26f94b4d5d46d7a45f
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-01-31 16:52:13 -0300

    crud para cliente

--- src/main/java/com/rodrigocalmon/cursomc/service/ClienteService.java
+++ src/main/java/com/rodrigocalmon/cursomc/service/ClienteService.java
@@ -1,11 +1,16 @@
 package com.rodrigocalmon.cursomc.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigocalmon.cursomc.domain.Cliente;
+import com.rodrigocalmon.cursomc.dto.ClienteDTO;
 import com.rodrigocalmon.cursomc.repository.ClienteRepository;
 import com.rodrigocalmon.cursomc.service.exception.ObjectNotFoundException;
 
@@ -19,5 +24,34 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		repo.deleteById(id);
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 }
