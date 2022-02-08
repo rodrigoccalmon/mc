17d8378f3bf3c9e5c66447ab284a05bb0b596b9d
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-02-08 16:47:12 -0300

    Thymeleaf, envio de e-mail html

--- src/main/java/com/rodrigocalmon/cursomc/service/EmailService.java
+++ src/main/java/com/rodrigocalmon/cursomc/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.rodrigocalmon.cursomc.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.rodrigocalmon.cursomc.domain.Pedido;
@@ -7,5 +9,11 @@ import com.rodrigocalmon.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
+	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+
 }
